24a86086ebb16dbcf644ae9e5e14b1e1297b5db9
KevinVillalobos <66274833+KevinVillalobos@users.noreply.github.com>
2022-04-08 11:59:58 -0600

    Add files via upload

--- pagina/database/database.sql
+++ pagina/database/database.sql
@@ -1,17 +1,114 @@
-CREATE TABLE `usuario` (
-  `name` varchar(50) NOT NULL,
-  `lastname` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-    `pass` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Apr 08, 2022 at 05:59 PM
+-- Server version: 8.0.28
+-- PHP Version: 8.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-INSERT INTO `usuario`(`name`, `lastname`, `email`, `pass`) VALUES ('admin','admin','admin@gmail.com','admin');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `producto` (
-  `id` int (11) AUTO_INCREMENT,  
-  `nombre` varchar(50) NOT NULL,
-  `tipo` varchar(50) NOT NULL,
-  `precio` varchar(50) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+-- Database: `noxus gym`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Fecha`
+--
+
+CREATE TABLE `Fecha` (
+  `id` int NOT NULL,
+  `Nombre` int NOT NULL,
+  `Apellido` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `membresias`
+--
+
+CREATE TABLE `membresias` (
+  `id` int NOT NULL,
+  `Nombre` text,
+  `Apellido` text,
+  `Edad` int DEFAULT NULL,
+  `Tipo` text,
+  `Fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `membresias`
+--
+
+INSERT INTO `membresias` (`id`, `Nombre`, `Apellido`, `Edad`, `Tipo`, `Fecha`) VALUES
+(3, 'Kevin', 'Villalobos', 20, 'Premium', '2022-04-08 17:59:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `Nombre` text,
+  `Apellido` text,
+  `Numero` int DEFAULT NULL,
+  `Fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `Nombre`, `Apellido`, `Numero`, `Fecha`) VALUES
+(5, 'Kevin', 'Villalobos', 123, '2022-04-08 17:58:28');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `membresias`
+--
+ALTER TABLE `membresias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `membresias`
+--
+ALTER TABLE `membresias`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
